60d0ce939ef0e2d5d1afd006bff8b08fa5a73434
Piyush Chauhan <chauhanpiyush@hotmail.com>
2018-06-15 12:58:05 +0530

    Loading csv files

--- Machine Learning Mastery with Python/Chapter 4/Load CSV Using Python Standard Library.py
+++ Machine Learning Mastery with Python/Chapter 4/Load CSV Using Python Standard Library.py
@@ -2,7 +2,7 @@
 import csv
 import numpy
 filename = 'pima-indians-diabetes.csv'
-raw_data = open(filename, 'rb')
+raw_data = open(filename, 'r')
 reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE)
 x = list(reader)
 data = numpy.array(x).astype('float')
