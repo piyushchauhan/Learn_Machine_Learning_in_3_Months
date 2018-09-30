66abb0fd07a28d2e15a202af7d1ae60de0ff92c0
Piyush Chauhan <chauhanpiyush@hotmail.com>
2018-09-30 15:57:26 +0530

    Added new Items
    Signed-off-by: Piyush Chauhan <chauhanpiyush@hotmail.com>

--- Machine Learning Mastery with Python/Chapter 4/Load CSV using NumPy.py
+++
@@ -1,6 +0,0 @@
-# Load CSV using NumPy
-from numpy import loadtxt
-filename = '../pima-indians-diabetes.csv'
-raw_data = open(filename, 'r')
-data = loadtxt(raw_data, delimiter=",")
-print(data.shape)
